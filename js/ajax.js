f82f4dbf60857e92c3e50f201d92e5f7b160aa84
lobabob <fkdaya@gmail.com>
2014-08-19 22:45:35 -0500

    forgot to delete extra stuff from ajax script

--- js/ajax.js
+++ js/ajax.js
@@ -27,12 +27,4 @@ setInterval(function(){
     $('#'+i+' > #runtime').html(data[i]['runtime']);
     $('#'+i+' > #eta').html(data[i]['eta']);
   }
-
- 
-
-  $('#stat_total').html(data['Total Users']);
-  $('#stat_today').html(data['Today']);
-  $('#stat_thirty_avg').html(data['Avg Users 30']);
-  $('#stat_ps_cs6').html(data['CS6']);
-  $('#stat_ps_cc').html(data['CC']);
 }, 1000);
