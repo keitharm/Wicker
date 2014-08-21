49f254a924e645923cf8c93b69c16335685e4fba
Keith Armstrong <keith1015@gmail.com>
2014-08-21 13:31:07 -0500

    Fixed minor bug with new uptime calculation

--- Wicker.php
+++ Wicker.php
@@ -355,7 +355,7 @@ class Wicker
         exec("sensors; uptime; du -hs uploads; du -hs logs;", $data);
         $cpus = array(substr($this->extractData($data[8], "+", " C")[0], 0, -2), substr($this->extractData($data[9], "+", " C")[0], 0, -2), substr($this->extractData($data[10], "+", " C")[0], 0, -2), substr($this->extractData($data[11], "+", " C")[0], 0, -2));
         $loadavgs = explode(", ", substr($data[13], strpos($data[13], "average: ")+9));
-        $uptime = array($this->extractData($data[13], "up ", ",  ") . " - " . $this->extractData($data[13], ",", ",")[0], $loadavgs[0], $loadavgs[1], $loadavgs[2]);
+        $uptime = array($this->extractData($data[13], "up ", ",") . " - " . $this->extractData($data[13], ",", ",")[0], $loadavgs[0], $loadavgs[1], $loadavgs[2]);
         $uploads = trim($this->extractData(":" . $data[14], ":", "uploads"));
         $logs = trim($this->extractData(":" . $data[15], ":", "logs"));
         return array("<span style='color: " . $this->color($cpus[0], 100, 60) . "'>" . $cpus[0] . "°C</span>", "<span style='color: " . $this->color($cpus[1], 100, 60) . "'>" . $cpus[1] . "°C</span>", "<span style='color: " . $this->color($cpus[2], 100, 60) . "'>" . $cpus[2] . "°C</span>", "<span style='color: " . $this->color($cpus[3], 100, 60) . "'>" . $cpus[3] . "°C</span>", $uptime[0], "<span style='color: " . $this->color($uptime[1], 4.00, 1.00) . "'>" . $uptime[1] . "</span>", "<span style='color: " . $this->color($uptime[2], 4.00, 1.00) . "'>" . $uptime[2] . "</span>", "<span style='color: " . $this->color($uptime[3], 4.00, 1.00) . "'>" . $uptime[3] . "</span>", $uploads, $logs);
