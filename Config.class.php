6bc1692a0c4d4bc97e77ce04c7f3eeba4d4be71f
Keith Armstrong <solewolf@users.noreply.github.com>
2014-08-21 15:03:48 -0500

    Cleared DB credentials
    Sorry, we changed our passwords anyway if you are looking at this :P

--- Config.class.php
+++ Config.class.php
@@ -1,8 +1,8 @@
 <?php
 class Config
 {
-    const DB_NAME = "wicker";
-    const DB_USER = "wicker";
-    const DB_PASS = "wicker";
+    const DB_NAME = "";
+    const DB_USER = "";
+    const DB_PASS = "";
 }
 ?>
