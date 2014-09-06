56e3b966d1803c73887b631541338b768c7e4c26
Keith Armstrong <keith1015@gmail.com>
2014-09-06 04:12:59 -0500

    Fixed CPU name reference. Starts from 1 instead of 0 now

--- fetch.php
+++ fetch.php
@@ -26,7 +26,7 @@ switch($type) {
         }
         break;
     case "system":
-        $name = array("CPU0", "CPU1", "CPU2", "CPU3", "Uptime", "1m", "5m", "15m", "Uploads", "Logs");
+        $name = array("CPU1 ", "CPU2", "CPU3", "CPU4", "Uptime", "1m", "5m", "15m", "Uploads", "Logs");
 
         for($i = 0; $i < count($name); $i++)
             $data[$name[$i]] = $wicker->status()[$i];
