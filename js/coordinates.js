16451b82c83dcce0ebe6e17b09da577cc1725deb
Keith Armstrong <keith1015@gmail.com>
2014-10-07 21:17:31 -0500

    Update scan even if coords aren't available for offline use

--- js/coordinates.js
+++ js/coordinates.js
@@ -7,7 +7,7 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(postCoordinates);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        $.post( "scanctl.php?do=coords&id=" + id, { lat: 0, long: 0 } );
     }
 }
 getLocation();
