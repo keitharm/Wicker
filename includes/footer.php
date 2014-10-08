dbc9c9b9a0e1baf52440f483a976d6d933b711b0
Keith Armstrong <keith1015@gmail.com>
2014-10-07 20:52:36 -0500

    Added local jQuery for offline use

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/jquery-2.1.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/holder.js"></script>
 <script src="js/ajax.js"></script>
