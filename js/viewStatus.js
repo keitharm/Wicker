428f33fc9433b9824041e41c472c00a4c0ad24f1
lobabob <fkdaya@gmail.com>
2014-10-27 15:49:48 -0500

    Progress bars should be completely fixed forever

--- js/viewStatus.js
+++ js/viewStatus.js
@@ -29,8 +29,8 @@ function getStatsData() {
                 }
 
                 // Progress bar updates
-                switch(data[i]['status']) {
-                    case '1':     // Cracking
+                switch(parseInt(data[i]['status'])) {
+                    case 1:     // Cracking
                         $('#'+i+' > #status .progress-bar').addClass('progress-bar-striped')
                             .removeClass('noStatus progress-bar-success progress-bar-warning progress-bar-danger')
                             .attr('aria-valuenow', data[i]['complete']).html(data[i]['complete']+'%');
@@ -38,22 +38,22 @@ function getStatsData() {
                         if(data[i]['complete'] > 20)
                             $('#'+i+' > #status .progress-bar').width(data[i]['complete']+'%');
                         break;
-                    case '2':     // Failed
+                    case 2:     // Failed
                         $('#'+i+' > #status .progress-bar').addClass('progress-bar-danger')
                             .removeClass('noStatus progress-bar-striped progress-bar-warning')
                             .attr('aria-valuenow', 50).removeAttr('style').html('Failed');
                         break;
-                    case '3':     // Success
+                    case 3:     // Success
                         $('#'+i+' > #status .progress-bar').addClass('progress-bar-success')
                             .removeClass('noStatus progress-bar-striped progress-bar-warning progress-bar-danger')
                             .attr('aria-valuenow', 50).removeAttr('style').html('Success');
                         break;
-                    case '4':     // Terminated
+                    case 4:     // Terminated
                         $('#'+i+' > #status .progress-bar').addClass('progress-bar-danger')
                             .removeClass('noStatus progress-bar-striped progress-bar-warning')
                             .attr('aria-valuenow', 50).removeAttr('style').html('Terminated');
                         break;
-                    case '5':     // Paused
+                    case 5:     // Paused
                         $('#'+i+' > #status .progress-bar').addClass('progress-bar-warning')
                             .removeClass('noStatus progress-bar-striped')
                             .attr('aria-valuenow', 50).removeAttr('style').html('Paused: '+data[i]['complete']+'%');
